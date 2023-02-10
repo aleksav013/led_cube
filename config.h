e1285b056a5083722850b48059895d6f8e2549e1
vmisovic <vladimir.misovic03@gmail.com>
2023-02-10 14:32:59 +0100

    map, config.h

---
+++ config.h
@@ -0,0 +1,15 @@
+// define the pins used controlling 74HC595
+#define DATA_PIN 2
+#define CLOCK_PIN 3
+#define LATCH_PIN 4
+
+// define the order of 74HC595 chain
+#define CHIP_0 0
+#define CHIP_1 1
+#define CHIP_2 3
+#define CHIP_3 5
+#define CHIP_4 7
+#define CHIP_5 8
+#define CHIP_6 6
+#define CHIP_7 4
+#define CHIP_8 2
